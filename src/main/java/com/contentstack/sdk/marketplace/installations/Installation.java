412a79340d21c3dbd1e6c8a99292cabb6262256a
Shailesh Mishra <mshaileshr@gmail.com>
2023-09-02 19:18:12 +0530

    enhanced code quality

--- src/main/java/com/contentstack/sdk/marketplace/installations/Installation.java
+++ src/main/java/com/contentstack/sdk/marketplace/installations/Installation.java
@@ -65,7 +65,7 @@ public class Installation implements BaseImplementation<Installation> {
     /**
      * The function validates if the installationId is not null or empty.
      *
-     * @param installationId The installationId parameter is a string that                       represents the unique identifier                       for an installation.
+     * @param installationId The installationId parameter is a string that represents the unique identifier for an installation.
      */
     protected void validateInstallationId(String installationId) {
         if (installationId == null || installationId.isEmpty()) {
@@ -78,11 +78,15 @@ public class Installation implements BaseImplementation<Installation> {
      *
      * @param client         the client
      * @param organisationId the organisation id
+     *                       <p>
+     *                       <b>Example</b>
+     *                       <p>
+     *                       <code>
+     *                       Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     *                       .host("api.contentstack.io").build();
+     *                       Installation installation = marketplace.installation();
+     *                       </code>
      */
-// The `public Installation(@NotNull Retrofit client, @NotNull String
-    // organisationId)` is a constructor
-    // for the `Installation` class. It takes two parameters: `client` and
-    // `organisationId`.
     public Installation(@NotNull Retrofit client, @NotNull String organisationId) {
         checkOrganisationId(organisationId);
         init(client, organisationId, null);
@@ -117,6 +121,17 @@ public class Installation implements BaseImplementation<Installation> {
      * The function returns a list of installed apps by making a network call.
      *
      * @return The method is returning a `Call` object with a generic type of `ResponseBody`.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.findInstalledApps().execute();
+     * </code>
      */
     Call<ResponseBody> findInstalledApps() {
         return this.service.listInstalledApps(this.headers, this.params);
@@ -126,6 +141,17 @@ public class Installation implements BaseImplementation<Installation> {
      * Find installations call.
      *
      * @return the call
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.findInstallations().execute();
+     * </code>
      */
     Call<ResponseBody> findInstallations() {
         return this.service.listInstallations(this.headers, this.params);
@@ -136,6 +162,17 @@ public class Installation implements BaseImplementation<Installation> {
      * making a network request.
      *
      * @return The method `fetchInstallation()` is returning a `Call<ResponseBody>` object.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.fetchInstallation().execute();
+     * </code>
      */
     Call<ResponseBody> fetchInstallation() {
         validateInstallationId(this.installationId);
@@ -148,6 +185,17 @@ public class Installation implements BaseImplementation<Installation> {
      * request.
      *
      * @return The method is returning a `Call<ResponseBody>` object.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.fetchInstallationData().execute();
+     * </code>
      */
     Call<ResponseBody> fetchInstallationData() {
         validateInstallationId(this.installationId);
@@ -158,8 +206,19 @@ public class Installation implements BaseImplementation<Installation> {
      * The function updates an installation with a given installation ID and returns
      * the response body.
      *
-     * @param body A JSONObject containing the data to be updated for the             installation.
+     * @param body A JSONObject containing the data to be updated for the installation.
      * @return The method is returning a Call object with a ResponseBody type.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.updateInstallation(body).execute();
+     * </code>
      */
     Call<ResponseBody> updateInstallation(JSONObject body) {
         validateInstallationId(this.installationId);
@@ -169,7 +228,18 @@ public class Installation implements BaseImplementation<Installation> {
     /**
      * The function returns a list of installed users by making a network call.
      *
-     * @return The method is returning a `Call` object with a generic type of `ResponseBody`.
+     * @return The method is returning a `Call` object with a generic type of `ResponseBody`
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.findInstalledUsers().execute();
+     * </code>
      */
     Call<ResponseBody> findInstalledUsers() {
         return this.service.listInstalledUsers(this.headers, this.params);
@@ -179,6 +249,17 @@ public class Installation implements BaseImplementation<Installation> {
      * The function returns a list of installed stacks by making a network call.
      *
      * @return The method is returning a `Call` object with a generic type of `ResponseBody`.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.findInstalledStacks().execute();
+     * </code>
      */
     Call<ResponseBody> findInstalledStacks() {
         return this.service.listInstalledStacks(this.headers, this.params);
@@ -190,6 +271,17 @@ public class Installation implements BaseImplementation<Installation> {
      * a service, returning the response body.
      *
      * @return The method is returning a Call object with a ResponseBody type.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.uninstall().execute();
+     * </code>
      */
     Call<ResponseBody> uninstall() {
         validateInstallationId(this.installationId);
@@ -202,6 +294,17 @@ public class Installation implements BaseImplementation<Installation> {
      * request.
      *
      * @return The method is returning a Call object with a generic type of ResponseBody.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.fetchAppConfiguration().execute();
+     * </code>
      */
     Call<ResponseBody> fetchAppConfiguration() {
         validateInstallationId(this.installationId);
@@ -214,6 +317,17 @@ public class Installation implements BaseImplementation<Installation> {
      * and parameters.
      *
      * @return The method is returning a Call object with a generic type of ResponseBody.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.fetchServerConfiguration().execute();
+     * </code>
      */
     Call<ResponseBody> fetchServerConfiguration() {
         validateInstallationId(this.installationId);
@@ -227,6 +341,17 @@ public class Installation implements BaseImplementation<Installation> {
      *
      * @param body A JSONObject containing the updated server configuration data.
      * @return The method is returning a Call object with a ResponseBody type.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.updateServerConfiguration().execute();
+     * </code>
      */
     Call<ResponseBody> updateServerConfiguration(JSONObject body) {
         validateInstallationId(this.installationId);
@@ -238,6 +363,17 @@ public class Installation implements BaseImplementation<Installation> {
      *
      * @param body A JSONObject containing the updated stack configuration data.
      * @return The method is returning a Call object with a ResponseBody type.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.updateStackConfiguration(body).execute();
+     * </code>
      */
     Call<ResponseBody> updateStackConfiguration(JSONObject body) {
         validateInstallationId(this.installationId);
@@ -250,6 +386,16 @@ public class Installation implements BaseImplementation<Installation> {
      * the service.
      *
      * @return The method is returning a `Call` object with a generic type of `ResponseBody`.
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * Call result = installation.createInstallationToken().execute();
+     * </code>
      */
     Call<ResponseBody> createInstallationToken() {
         validateInstallationId(this.installationId);
@@ -262,6 +408,17 @@ public class Installation implements BaseImplementation<Installation> {
      * installationId.
      *
      * @return An instance of the Location class.
+     *
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * Call result = installation.location().execute();
+     * </code>
      */
     public Location location() {
         validateInstallationId(this.installationId);
@@ -274,6 +431,18 @@ public class Installation implements BaseImplementation<Installation> {
      *
      * @param webhookId A string representing the ID of the webhook.
      * @return An instance of the Webhook class.
+     *
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation();
+     * JSONObject body = new JSONObject();
+     * Call result = installation.webhook("webhookId").execute();
+     * </code>
      */
     public Webhook webhook(@NotNull String webhookId) {
         validateInstallationId(this.installationId);
@@ -281,24 +450,77 @@ public class Installation implements BaseImplementation<Installation> {
     }
 
 
+    /**
+     * @param key   A non-null String representing the key for the parameter.
+     * @param value The value parameter is of type Object, which means it can accept              any type of object              as its value.
+     * @return instance of Installation
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation().addParam("key", "value");
+     * </code>
+     */
     @Override
     public Installation addParam(@NotNull String key, @NotNull Object value) {
         this.params.put(key, value);
         return this;
     }
 
+    /**
+     * @param key   A non-null String representing the header key.
+     * @param value The value parameter is a string that represents the value of the              header.
+     * @return instance of Installation
+     *
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * Installation installation = marketplace.installation().addHeader("key", "value");
+     * </code>
+     */
     @Override
     public Installation addHeader(@NotNull String key, @NotNull String value) {
         this.headers.put(key, value);
         return this;
     }
 
+    /**
+     * @param parameters A HashMap object that stores key-value pairs, where the key                   is of type String                   and the value is of type Object. The HashMap is annotated                   with @NotNull, indicating that it                   cannot be null.
+     * @return instance of {@link Installation}
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * HashMap parameters = new HashMap();
+     * Installation installation = marketplace.installation().addParams(parameters);
+     * </code>
+     */
     @Override
     public Installation addParams(@NotNull HashMap<String, Object> parameters) {
         this.params.putAll(parameters);
         return this;
     }
 
+    /**
+     * @param headers A HashMap object that contains key-value pairs representing                the headers to be                added. The keys represent the header names, and the values                represent the header values.
+     * @return instance of {@link Installation}
+     * <p>
+     * <b>Example</b>
+     * <p>
+     * <code>
+     * Marketplace marketplace = new Marketplace.Builder("ORGANIZATION_UID")
+     * .host("api.contentstack.io").build();
+     * HashMap parameters = new HashMap();
+     * Installation installation = marketplace.installation().addHeaders(parameters);
+     * </code>
+     */
     @Override
     public Installation addHeaders(@NotNull HashMap<String, String> headers) {
         this.headers.putAll(headers);
